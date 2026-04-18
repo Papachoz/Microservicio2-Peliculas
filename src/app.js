0aa3bce87c9b74ec121d45bb6b60cfbdb68a26d4
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-04-17 19:18:17 -0500

    App

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import swagger from '@fastify/swagger'
 import swaggerUi from '@fastify/swagger-ui'
 import 'dotenv/config'
 import { initDB } from './db/database.js'
-
+import * as database from './db/database.js'  
 import movieRoutes    from './modules/movies/movie.routes.js'
 import directorRoutes from './modules/directors/director.routes.js'
 import genreRoutes    from './modules/genres/genre.routes.js'
@@ -14,7 +14,6 @@ import reviewRoutes   from './modules/reviews/review.routes.js'
 const fastify = Fastify({ logger: true })
 
 await fastify.register(cors)
-
 await fastify.register(swagger, {
   openapi: {
     info: {
@@ -22,9 +21,8 @@ await fastify.register(swagger, {
       description: 'API REST para gestión de películas',
       version: '1.0.0'
     }
-  }
+  }                              
 })
-
 await fastify.register(swaggerUi, {
   routePrefix: '/docs'
 })
@@ -35,13 +33,26 @@ fastify.register(movieRoutes,    { prefix: '/api' })
 fastify.register(directorRoutes, { prefix: '/api' })
 fastify.register(genreRoutes,    { prefix: '/api' })
 fastify.register(actorRoutes,    { prefix: '/api' })
-fastify.register(reviewRoutes,   { prefix: '/api' })
+fastify.register(reviewRoutes,   { prefix: '/api' })  
 
 fastify.get('/health', async () => ({ status: 'ok' }))
 
-try {
+fastify.get('/api/todos_los_registros', async (request, reply) => {
+  const [[movies], [directors], [genres], [actors], [reviews], [movie_directors], [movie_genres], [movie_actors]] = await Promise.all([
+    database.db.query('SELECT * FROM movies'),       
+    database.db.query('SELECT * FROM directors'),
+    database.db.query('SELECT * FROM genres'),
+    database.db.query('SELECT * FROM actors'),
+    database.db.query('SELECT * FROM reviews'),
+    database.db.query('SELECT * FROM movie_directors'),
+    database.db.query('SELECT * FROM movie_genres'),
+    database.db.query('SELECT * FROM movie_actors'),
+  ])
+  return { movies, directors, genres, actors, reviews, movie_directors, movie_genres, movie_actors }
+})
+
+try {                                            
   await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' })
 } catch (err) {
   fastify.log.error(err)
-  process.exit(1)
-}
+  process.exit(
